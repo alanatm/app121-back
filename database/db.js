d3e5ee2a71e08b24b76ded53e59ce4e39ebb2ce5
Alan <roalan96@gmail.com>
2024-01-24 23:02:54 -0600

    se agrega variable entorno url

--- database/db.js
+++ database/db.js
@@ -4,9 +4,11 @@ import {
     DB_PORT,
     DB_DATABASE,
     DB_USER,
-    DB_PASSWORD
+    DB_PASSWORD,
+    DB_URL
 } from '../routes/config.js';
 
+const sequelize = new Sequelize(DB_URL);
 const db = new Sequelize(DB_DATABASE, DB_USER,DB_PASSWORD,{
     host: DB_HOST,
     port: DB_PORT,

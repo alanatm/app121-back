dd31a03b482e4d4dcfed19c4b866fa1cd39c2870
Alan <roalan96@gmail.com>
2024-01-25 21:52:33 -0600

    ssl

--- database/db.js
+++ database/db.js
@@ -4,12 +4,13 @@ import {
     DB_DATABASE,
     DB_USER,
     DB_PASSWORD,
+    MYSQL_ATTR_SSL_CA
 } from '../routes/config.js';
 
 const db = new Sequelize(DB_DATABASE, DB_USER,DB_PASSWORD,{
     host: DB_HOST,
-    dialect: 'mysql'
-    
+    dialect: 'mysql',
+    ssl:MYSQL_ATTR_SSL_CA 
 });
 
 export default db;
--- routes/config.js
+++ routes/config.js
@@ -7,3 +7,4 @@ export const DB_USER = '13linkaajsi4d4o890nn';
 export const DB_PASSWORD = 'pscale_pw_LhRtBorVnf2qVdSH3JoFsE7VgbixGc6dLvQmUkA3dhB';
 export const DB_HOST = 'aws.connect.psdb.cloud';
 export const DB_DATABASE= 'contactcenter121';
+export const MYSQL_ATTR_SSL_CA=/etc/ssl/cert.pem;
